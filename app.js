1a896d75b522fccec210139a61e18677acbf4865
Raffael Alves <raffa.aqr.pt@gmail.com>
2025-07-04 17:31:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+alert('Boas vindas ao nosso site!');
+let idade = prompt ('qual sua idade');
+if (idade >= 18) {
+    alert('pode tirar habilitação');
+} else {
+    alert('Não pode tirar habilitação');
+}
+
+
